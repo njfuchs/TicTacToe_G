233ea99d271bd888e87ffe3ee2fa4d41d3fcd977
nfuchs03 <nfuchs@student.tgm.ac.at>
2024-06-12 16:09:48 +0200

    add basic Class and Method structure based on Class Diagramm

---
+++ src/main/java/org/example/Board.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Board {
+    private char[][] cells;
+    public Board(){
+
+    }
+
+    public boolean isCellEmpty(int x, int y){
+        return false
+    }
+
+    public void place (int x, int y, char c){
+
+    }
+
+    public boolean isFull(){
+        return false;
+    }
+
+    public void clear (){
+
+    }
+
+    public void print(){
+
+    }
+}
+
---
+++ src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class TicTacToe {
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private Board board;
+    public TicTacToe(){
+
+    }
+    public void start(){
+
+    }
+    private void switchPlayer(){
+
+    }
+
+    private boolean hasWinner(){
+
+        return false;
+    }
+}

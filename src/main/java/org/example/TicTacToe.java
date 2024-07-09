04e2e5ce01c4e7376357783293ff83fa3eca52fa
nfuchs03 <nfuchs@student.tgm.ac.at>
2024-07-09 23:13:03 +0200

    added Unit Test for Methods now min. 2 per Method

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -5,45 +5,40 @@ import java.util.Scanner;
 public class TicTacToe {
     private Player player1;
     private Player player2;
-    private Player currentPlayer;
     private Board board;
-
+    private Scanner sc = new Scanner(System.in);
     public TicTacToe() {
         player1 = new Player('X');
         player2 = new Player('O');
-        currentPlayer = player1;
         board = new Board();
     }
 
-    public void placeX (){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Player X: wich row? 0, 1 or 2");
-        int r = sc.nextInt();
-        System.out.println("Player X: wich column? 0, 1 or 2");
-        int c = sc.nextInt();
-        board.place(r, c, 'X');
-        sc.close();
+    public void place (char player){
+        for (boolean changed = false; changed==false;) {
+            System.out.println("Player" + player + ": wich row? 0, 1 or 2");
+            int r = sc.nextInt();
+            System.out.println("Player" + player + ": wich column? 0, 1 or 2");
+            int c = sc.nextInt();
+            if (r<0||c<0||r>2||c>2||!board.isCellEmpty(r, c)) {
+                System.out.println(player+" Dieses Feld ist ungültig bitte neues Wählen!");
+            }else {
+                board.place(r, c, player);
+                changed = true;
+            }
+        }
+
     }
-    public void placeO (){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Player O: wich row? 0, 1 or 2");
-        int r = sc.nextInt();
-        System.out.println("Player O: wich column? 0, 1 or 2");
-        int c = sc.nextInt();
-        board.place(r, c, 'O');
-        sc.close();
-    }
+
     public void start() {
-        Scanner sc = new Scanner(System.in);
         boolean b = true;
         for (boolean s = true; s;) {
             board.clear();
             for (int i = 0; i < 9; i++) {
                 if (b) {
-                    placeX();
+                    place(player1.getMarker());
                     b = false;
                 } else {
-                    placeO();
+                    place(player2.getMarker());
                     b = true;
                 }
                 board.print();
@@ -51,8 +46,8 @@ public class TicTacToe {
                     System.out.println("Es gibt einen Gewinner das Spiel ist vorbei!");
                     System.out.println("Neues Spiel starten? 1 = Ja 0 = nein");
                     s = sc.nextInt() == 1;
-                    i = 10;
                 }
+                System.out.println(i);
             }
             if (!board.hasWinner()) {
                 System.out.println("Es gibt keinen Gewinner! Unentschieden!");
--- src/main/java/test/TestUnitTestPlayer.java
+++ src/main/java/test/TestUnitTestPlayer.java
@@ -13,7 +13,23 @@ public class TestUnitTestPlayer {
     }
 
     @Test
-    public void getMarkerTest (){
+    public void getMarkerTestX(){
         assertEquals(player.getMarker(), 'X');
     }
+    @Test
+    public void getMarkerTestXFalse(){
+        player = new Player('O');
+        assertFalse(player.getMarker() == 'X');
+    }
+    @Test
+    public void getMarkerTestO(){
+        player = new Player('O');
+        assertEquals(player.getMarker(), 'O');
+    }
+    @Test
+    public void getMarkerTestOFalse(){
+        player = new Player('X');
+        assertFalse(player.getMarker()=='O');
+    }
+
 }

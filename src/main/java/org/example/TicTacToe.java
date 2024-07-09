593147de9c2feb53f70f189d1871e31a07a21cda
nfuchs03 <nfuchs@student.tgm.ac.at>
2024-07-09 18:19:41 +0200

    add User Stroy 4, the Player can now restart the game if wished.

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -18,31 +18,44 @@ public class TicTacToe {
     public void start() {
         Scanner sc = new Scanner(System.in);
         boolean b = true;
-        for (int i = 0; i < 9; i++){
-            if (b){
-                System.out.println("Player X: wich row? 0, 1 or 2");
-                int r = sc.nextInt();
-                System.out.println("Player X: wich column? 0, 1 or 2");
-                int c = sc.nextInt();
-                board.place(r, c, 'X');
-                b = false;
-                board.print();
-            } else {
-                System.out.println("Player O: wich row? 0, 1 or 2");
-                int r = sc.nextInt();
-                System.out.println("Player O: wich column? 0, 1 or 2");
-                int c = sc.nextInt();
-                board.place(r, c, 'O');
-                b = true;
-                board.print();
+        for (boolean s = true; s;) {
+            board.clear();
+            for (int i = 0; i < 9; i++) {
+                if (b) {
+                    System.out.println("Player X: wich row? 0, 1 or 2");
+                    int r = sc.nextInt();
+                    System.out.println("Player X: wich column? 0, 1 or 2");
+                    int c = sc.nextInt();
+                    board.place(r, c, 'X');
+                    b = false;
+                    board.print();
+                } else {
+                    System.out.println("Player O: wich row? 0, 1 or 2");
+                    int r = sc.nextInt();
+                    System.out.println("Player O: wich column? 0, 1 or 2");
+                    int c = sc.nextInt();
+                    board.place(r, c, 'O');
+                    b = true;
+                    board.print();
+                }
+                if (board.hasWinner()) {
+                    System.out.println("Es gibt einen Gewinner das Spiel ist vorbei,  Neues Spiel starten? 1 = Ja 0 = nein");
+                    if (sc.nextInt()==1){
+                        s = true;
+                    }else{
+                        s = false;
+                    }
+                    i = 10;
+                }
             }
-            if (board.hasWinner()){
-                System.out.println("Es gibt einen Gewinner das Spiel ist vorbei");
-                i = 10;
+            if (!board.hasWinner()) {
+                System.out.println("Es gibt keinen Gewinner! Unentschieden! Neues Spiel starten? 1 = Ja 0 = nein");
+                if (sc.nextInt()==1){
+                    s = true;
+                } else  {
+                    s = false;
+                }
             }
-        }
-        if (!board.hasWinner()){
-            System.out.println("Es gibt keinen Gewinner! Unentschieden");
         }
     }
 
